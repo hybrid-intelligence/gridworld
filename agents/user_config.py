a997e86665f5e7183567e3be8dcf9546a2eb5a27
zoya <zoya.v@ya.ru>
2022-08-24 19:38:36 +0800

    MHB agent

--- agents/user_config.py
+++ agents/user_config.py
@@ -1,15 +1,15 @@
 
 from agents.random_agent import RandomBuilderAgent
-
+from agents.mhb_baseline.agent import APPOAgent
 ###################################################################
 #####        Specify your agent and other configs here        #####
 ###################################################################
 
 class UserConfig:
-    SingleAgent = RandomBuilderAgent
+    SingleAgent = APPOAgent
 
     # Only select between 'walking' and 'flying' (Invalid ones will default to walking)
-    ActionSpaceName = 'flying'
+    ActionSpaceName = 'walking'
 
     # Check agents/user_config_vector_agent.py if you want to use multiple environments in parallel
     # to process a batch of observations at once

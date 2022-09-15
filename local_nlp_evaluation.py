db3f9b5ee2676e92fc3e08b5cc1b6a72c636f127
tviskaron <tviskaron@gmail.com>
2022-09-15 13:53:26 +0300

    user-friendly script for nlp eval

---
+++ local_nlp_evaluation.py
@@ -0,0 +1,48 @@
+import os
+
+import numpy as np
+
+from agents.mhb_baseline.nlp_model.agent import get_dialog, GridPredictor
+from agents.mhb_baseline.nlp_model.utils import plot_voxel, compute_metric
+
+
+def main():
+    grid_predictor = None
+
+    # os.environ['IGLU_DATA_PATH'] = 'iglu_data'
+
+    from gridworld.tasks import Task
+
+    from gridworld.data import IGLUDataset
+    dataset = IGLUDataset(task_kwargs=None, force_download=False, )
+
+    total_score = []
+
+    for j, (task_id, session_id, subtask_id, subtask) in enumerate(dataset):
+        str_id = str(task_id) + '-session-' + str(session_id).zfill(3) + '-subtask-' + str(subtask_id).zfill(3)
+        print('Starting task:', str_id)
+        subtask: Task = subtask
+
+        if grid_predictor is None:
+            grid_predictor = GridPredictor()
+
+        dialog = get_dialog(subtask)
+        predicted_grid = grid_predictor.predict_grid(dialog)
+
+        if not os.path.exists('plots'):
+            os.makedirs('plots')
+
+        f1_score = round(compute_metric(predicted_grid, subtask.target_grid)['completion_rate_f1'], 3)
+        results = {'F1': f1_score}
+        total_score.append(f1_score)
+        results_str = " ".join([f"{metric}: {value}" for metric, value in results.items()])
+        plot_voxel(predicted_grid, text=str_id + ' ' + f'({results_str})' + "\n" + dialog).savefig(
+            f'./plots/{str_id}-predicted.png')
+        plot_voxel(subtask.target_grid, text=str_id + " (Ground truth)\n" + dialog).savefig(
+            f'./plots/{str_id}-gt.png')
+
+    print('Total F1 score:', np.mean(total_score))
+
+
+if __name__ == '__main__':
+    main()
